8750e4c18b3f1db2346fabdc705eb6a006a6085e
Juan Sierra <jasierra.garcia@gmail.com>
2017-04-15 00:32:53 -0400

    Add tons of content and refactor some css and html code

--- scripts/index.js
+++ scripts/index.js
@@ -2,4 +2,9 @@ $(document).ready(function () {
     $('#hamburger').click(function () {
         $('.sidebar').toggleClass('open');
     });
+    if (!$('.sidebar').hasClass('open')) {
+        $('.nav-link').click(function () {
+            $('.sidebar').removeClass('open');
+        })
+    }
 });

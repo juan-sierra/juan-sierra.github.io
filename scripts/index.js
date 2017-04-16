f98c90765bfe477104edac4f0eb7bf26fa3c89a8
Juan Sierra <jasierra.garcia@gmail.com>
2017-04-15 22:30:00 -0400

    Add scroll on click

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    // sidebar functionality
     $('#hamburger').click(function () {
         $('.sidebar').toggleClass('open');
     });
@@ -7,4 +8,22 @@ $(document).ready(function () {
             $('.sidebar').removeClass('open');
         })
     }
+
+    // scroll on click
+
+    // about = 646
+    // projects = 1572
+    // contact = 485
+    $('a[href^="#"]').click(function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').animate({
+            scrollTop: $target.offset().top
+        }, 1000, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
 });

12a0e669149091a61d51208fd05140f754624210
Juan Sierra <jasierra.garcia@gmail.com>
2017-07-20 22:38:02 -0400

    Gulp process setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,63 @@
-const gulp = require("gulp");
+var gulp,
+    sass,
+    uglify,
+    pump,
+    pug,
+    rename,
+    bs,
+    bsStream,
+    reload;
 
-// pug compiler
-const pug = require("gulp-pug");
+gulp = require("gulp");
+sass = require("gulp-sass");
+uglify = require("gulp-uglify");
+pump = require("pump");
+pug = require("gulp-pug");
+rename = require("gulp-rename");
+bs = require("browser-sync").create();
 
-// sass compiler
-const sass = require("gulp-sass");
-
-// minifier
-const uglify = require("gulp-uglify");
-
-// browser-sync
-const browserSync = require("browser-sync").create();
-
-// better error handler
-const pump = require("pump");
-
-// gulp rename files
-const rename = require("gulp-rename");
-
-// minify javascrpt
-gulp.task("minify", function () {
-    pump([
-        gulp.src("js/*.js"),
-        uglify(),
-        gulp.dest("src/js")
-    ]);
+gulp.task("sass", function () {
+    return gulp.src("./sass/**.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("./src/css"))
+        .pipe(bs.reload({
+            stream: true
+        }));
 });
 
-// pug compiler
-gulp.task("pug", function buildHTML() {
-    gulp.src("./**.pug")
+gulp.task("uglify", function (cb) {
+    pump([
+        gulp.src("./js/**.js"),
+        uglify(),
+        gulp.dest("./src/js"),
+        bs.reload({
+            stream: true
+        })
+    ], cb);
+});
+
+gulp.task("pug", function () {
+    return gulp.src("./**.pug")
         .pipe(pug({
             pretty: true
         }))
         .pipe(rename({
             basename: "index"
         }))
-        .pipe(gulp.dest("./"));
-})
+        .pipe(gulp.dest("./"))
+        .pipe(bs.reload({
+            stream: true
+        }));
+});
 
-// browser-sync dev server
-gulp.task("browser-sync", function() {
-    browserSync.init({
-        server: {
-            baseDir: "./",
-            index: "index.html",
-            files: "./**"
-        }
+gulp.task("server", ["sass", "uglify", "pug"], function () {
+    bs.init({
+        server: "./"
     });
+    gulp.watch("./sass/**.scss", ["sass"]);
+    gulp.watch("./js/**.js", ["uglify"]);
+    gulp.watch("./**.pug", ["pug"]);
+    gulp.watch("./**.html").on("change", bs.reload);
 });
 
-// sass compiler
-gulp.task("sass", function() {
-    gulp.src("sass/*.scss")
-        .pipe(sass().on("error", sass.logError))
-        .pipe(gulp.dest("src/css"));
-});
-
-gulp.task("watch", function() {
-    gulp.watch(["js/*.js", "sass/*.scss", "./**.pug"], ["minify", "sass", "pug"]);
-});
-
-gulp.task("default", ["watch", "browser-sync"]);
+gulp.task("default", ["sass", "uglify", "pug", "server"]);

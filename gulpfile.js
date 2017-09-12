1570782d5d36a109be4c603f3de2cd8a7d076b60
Juan Sierra <jasierra.garcia@gmail.com>
2017-09-12 00:30:09 -0400

    Cleaned up sections and finished contact form

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,4 @@
-var gulp,
-    sass,
-    uglify,
-    pump,
-    pug,
-    rename,
-    bs,
-    bsStream,
-    reload,
-    ap,
-    concat;
+var gulp, sass, uglify, pump, pug, rename, bs, bsStream, reload, ap, concat;
 
 gulp = require("gulp");
 sass = require("gulp-sass");
@@ -20,49 +10,62 @@ bs = require("browser-sync").create();
 autoprefixer = require("autoprefixer");
 concat = require("gulp-concat");
 
-gulp.task("sass", function () {
-    return gulp.src("./sass/imports.scss")
-        .pipe(sass())
-        .pipe(concat("style.css"))
-        .pipe(gulp.dest("./dist/css"))
-        .pipe(bs.reload({
-            stream: true
-        }));
+gulp.task("sass", function() {
+  return gulp
+    .src("./sass/imports.scss")
+    .pipe(sass())
+    .pipe(concat("style.css"))
+    .pipe(gulp.dest("./dist/css"))
+    .pipe(
+      bs.reload({
+        stream: true
+      })
+    );
 });
 
-gulp.task("uglify", function (cb) {
-    pump([
-        gulp.src("./js/**.js"),
-        uglify(),
-        gulp.dest("./dist/js"),
-        bs.reload({
-            stream: true
-        })
-    ], cb);
+gulp.task("uglify", function(cb) {
+  pump(
+    [
+      gulp.src("./js/**.js"),
+      uglify(),
+      gulp.dest("./dist/js"),
+      bs.reload({
+        stream: true
+      })
+    ],
+    cb
+  );
 });
 
-gulp.task("pug", function () {
-    return gulp.src("./**.pug")
-        .pipe(pug({
-            pretty: true
-        }))
-        .pipe(rename({
-            basename: "index"
-        }))
-        .pipe(gulp.dest("./"))
-        .pipe(bs.reload({
-            stream: true
-        }));
+gulp.task("pug", function() {
+  return gulp
+    .src("./**.pug")
+    .pipe(
+      pug({
+        pretty: true
+      })
+    )
+    .pipe(
+      rename({
+        basename: "index"
+      })
+    )
+    .pipe(gulp.dest("./"))
+    .pipe(
+      bs.reload({
+        stream: true
+      })
+    );
 });
 
-gulp.task("server", ["sass", "uglify", "pug"], function () {
-    bs.init({
-        server: "./"
-    });
-    gulp.watch("./sass/**.scss", ["sass"]);
-    gulp.watch("./js/**.js", ["uglify"]);
-    gulp.watch("./**.pug", ["pug"]);
-    gulp.watch("./**.html").on("change", bs.reload);
+gulp.task("server", ["sass", "uglify", "pug"], function() {
+  bs.init({
+    server: "./"
+  });
+  gulp.watch("./sass/**.scss", ["sass"]);
+  gulp.watch("./js/**.js", ["uglify"]);
+  gulp.watch("./**.pug", ["pug"]);
+  gulp.watch("./**.html").on("change", bs.reload);
 });
 
 gulp.task("default", ["sass", "uglify", "pug", "server"]);

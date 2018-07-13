4543ac5f3dc567a2d3a3315080ccf97610a82b2e
Juan Sierra <jasierra.garcia@gmail.com>
2018-07-12 22:42:18 -0500

    installed gatsby and styled components

---
+++ portfolio/src/components/Projects.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { H1 } from "../layouts/matrix"
+import styled from "styled-components"
+
+const Projects = () => (
+    <div>
+        <ProjectName>hello</ProjectName>
+    </div>
+)
+
+const ProjectName = styled(H1)`
+    font-size: 20rem;
+    font-family: Arial;
+    font-weight: lighter;
+`
+
+export default Projects
---
+++ portfolio/src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Projects from "../components/Projects"
+
+const IndexPage = () => (
+  <div>
+    <Projects />
+  </div>
+)
+
+export default IndexPage

34584e6be5254e783c53137d85a5a99b0fe5f2a4
Juan Sierra <jasierra.garcia@gmail.com>
2018-07-24 23:30:00 -0500

    Added font InterUI

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,17 +1,10 @@
 import React from "react"
-import { H1 } from "../layouts/matrix"
 import styled from "styled-components"
 
 const Projects = () => (
     <div>
-        <ProjectName>projects</ProjectName>
+
     </div>
 )
-
-const ProjectName = styled(H1)`
-    font-size: 5rem;
-    font-family: Arial;
-    font-weight: lighter;
-`
 
 export default Projects
--- portfolio/src/pages/index.js
+++ portfolio/src/pages/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Projects from "../components/Projects"
 import Nav from "../components/Nav"
 import About from "../components/About"
+import { Container } from "../layouts/matrix"
 
 const IndexPage = () => (
-  <div>
+  <Container>
     <About />
     <Projects />
     <Nav />
-  </div>
+  </Container>
 )
 
 export default IndexPage

bb8096c648f7d6648004003107344f114598171a
Juan Sierra <jasierra.garcia@gmail.com>
2018-09-26 17:43:17 -0400

    removing cache folder

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/jsierra/code/juan-sierra.github.io/node_modules/gatsby-plugin-offline/gatsby-browser'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/Users/jsierra/code/juan-sierra.github.io/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]

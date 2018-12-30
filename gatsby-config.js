be227316c604b420cffa7591022d6c520c2d1965
juan-sierra <jasierra.garcia@gmail.com>
2018-12-30 03:43:55 -0500

    Added typography js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,35 +1,31 @@
 module.exports = {
   siteMetadata: {
-    title: 'Juan Sierra'
+    title: "Juan Sierra"
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-react-helmet",
     `gatsby-plugin-styled-components`,
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui'
+        name: "gatsby-starter-default",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui"
       }
     },
     {
-      resolve: `gatsby-plugin-google-fonts`,
+      resolve: "gatsby-plugin-typography",
       options: {
-        fonts: [
-          `Domine`,
-          `Open Sans`,
-          `Serif\:300,400,400i,700` // you can also specify font weights and styles
-        ]
+        pathToConfigModuel: "src/utils/typography"
       }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
   ]
-}
+};

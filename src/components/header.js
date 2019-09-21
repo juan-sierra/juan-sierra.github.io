6ed6da353f7a18d5b503df339cd6b5dd3b300484
juan-sierra <juansierradev@gmail.com>
2019-09-21 02:01:16 -0400

    Small changes, added styled-components, started adding content

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,36 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import { Link } from "gatsby";
+import PropTypes from "prop-types";
+import React from "react";
+
+import styled from "styled-components";
+
+const H1 = styled.h1`
+    font-size: 5em;
+    color: pink;
+`;
+
+const Ul = styled.ul``;
+
+const ListItem = styled.li``;
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+    <header>
+        <H1>{siteTitle}</H1>
+        <Ul>
+            <ListItem>
+                <Link to="/">home</Link>
+            </ListItem>
+            <ListItem>
+                <Link>portfolio</Link>
+            </ListItem>
+            <ListItem>
+                <Link>resume</Link>
+            </ListItem>
+            <ListItem>
+                <Link>contact</Link>
+            </ListItem>
+        </Ul>
+    </header>
+);
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
+export default Header;
